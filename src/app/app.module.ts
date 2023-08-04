85d49f458bfed26fa7999870994044871077aa86
Renato Dias <renatocardosodias@icloud.com>
2023-08-04 18:30:16 -0300

    criando o formulário de login, adicionando font awesome ao projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 //PRIMENG
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './views/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    CardModule
+    CardModule,
+    InputTextModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
